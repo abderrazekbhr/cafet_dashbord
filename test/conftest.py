ff3dc763c2ee1307d6986be8a988856e070758d0
abderrazek123 <abderrazek455@gmail.com>
2024-02-26 00:12:50 +0100

    add test folder

---
+++ test/conftest.py
@@ -0,0 +1,13 @@
+import unittest
+
+class MyTest(unittest.TestCase):
+    def test_something(self):
+        # Write test cases here
+        pass
+
+    def test_another_thing(self):
+        # Write more test cases here
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
